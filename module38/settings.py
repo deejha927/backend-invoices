f7f379864bc629ea5c6c180a13447ecf5a9a0176
ransom12 <deejha927@gmail.com>
2023-12-16 11:53:17 +0530

    This is my second commit

--- module38/settings.py
+++ module38/settings.py
@@ -127,6 +127,6 @@ REST_FRAMEWORK = {"DEFAULT_AUTHENTICATION_CLASSES": ("rest_framework_simplejwt.a
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
